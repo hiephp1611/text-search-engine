a31570e2fdf255a74601e122fbfc502ceea16846
hiephp1611 <89740129+HunghiepLe@users.noreply.github.com>
2024-07-09 20:12:43 +0200

    Update searchEngine.py

--- searchEngine.py
+++ searchEngine.py
@@ -2,6 +2,8 @@ import string
 import nltk
 from nltk.tokenize import TreebankWordTokenizer
 from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 REMOVE_PUNCTUATION_TABLE= str.maketrans({x: None for x in string.punctuation})
 TOKENIZER = TreebankWordTokenizer()
@@ -25,8 +27,20 @@ example_doc_tokenized = TOKENIZER.tokenize( example_doc.translate(REMOVE_PUNCTUA
 
 # Strip words of their plural forms, formatting, etc.
 example_doc_tokenized_and_stemmed = [STEMMER.stem(token) for token in example_doc_tokenized]
-print(example_doc_tokenized_and_stemmed)
+
 # Transform a user query into a list of terms:
 def tokenize_and_stem(s):
     return [STEMMER.stem(t) for t
             in TOKENIZER.tokenize(s.translate(REMOVE_PUNCTUATION_TABLE))]
+
+# Gather vocabulary across the entire collection of documents by calling the fit method
+vectorizer = TfidfVectorizer(tokenizer = tokenize_and_stem, stop_words = 'english')
+vectorizer.fit(docs)
+
+query = 'contact email to chat to martin'
+query_vector = vectorizer.transform([query]).todense()
+
+# Calculate cosine distance between the query vector and all vectors for all documents  in collection
+doc_vectors = vectorizer.transform(docs)
+similarity = cosine_similarity(query_vector,doc_vectors)
+print(similarity)
